8bc6c6ab7360b9694490a21648c89c29ba973ace
Polina Drykova <polina.drykova1988@gmail.com>
2019-10-22 15:01:05 +0800

    functionality works, no style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  # GET /tasks:
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/id :
+  def show
+  end
+
+  # GET /tasks/new :
+  def new
+    @task = Task.new
+  end
+
+  # POST /tasks :
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  # GET /tasks/:id/edit :
+  def edit
+  end
+
+  # PATCH /tasks/:id :
+  def update
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  # DELETE /tasks/:id :
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Adding CRUD routs:
+  # verb "/somewhere", to: "controllerS#action", alis
+  # Read all taks:
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # Read one task:
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # Create:
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+  # Update:
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # Delete:
+  # delete '/tasks/:id', to: 'tasks#destroy'
 end
